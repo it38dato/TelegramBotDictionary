033ad0ac1285e98e5d7f24109557ba730e0e6b04
Dato <156312606+it38dato@users.noreply.github.com>
2024-07-24 21:50:00 +0800

    Update pyTranslatorBot1.py with err

--- pyTranslatorBot1.py
+++ pyTranslatorBot1.py
@@ -69,3 +69,65 @@ def bot_message(message):
 #    bot.send_message(message.chat.id, translated_text)
 
 bot.polling(none_stop=True)
+
+
+
+
+
+
+
+
+Ошибка:
+Traceback (most recent call last):
+  File "/TelegramBotDictionary/pyTranslatorBot1.py", line 70, in <module>
+    bot.polling(none_stop=True)
+  File "/TelegramBotDictionary/tenv/lib/python3.10/site-packages/telebot/__init__.py", line 1178, in polling
+    self.__threaded_polling(non_stop=non_stop, interval=interval, timeout=timeout, long_polling_timeout=long_polling_timeout,
+  File "/TelegramBotDictionary/tenv/lib/python3.10/site-packages/telebot/__init__.py", line 1253, in __threaded_polling
+    raise e
+  File "/TelegramBotDictionary/tenv/lib/python3.10/site-packages/telebot/__init__.py", line 1215, in __threaded_polling
+    self.worker_pool.raise_exceptions()
+  File "/TelegramBotDictionary/tenv/lib/python3.10/site-packages/telebot/util.py", line 150, in raise_exceptions
+    raise self.exception_info
+  File "/TelegramBotDictionary/tenv/lib/python3.10/site-packages/telebot/util.py", line 93, in run
+    task(*args, **kwargs)
+  File "/TelegramBotDictionary/tenv/lib/python3.10/site-packages/telebot/__init__.py", line 8707, in _run_middlewares_and_handler       
+    result = handler['function'](message)
+  File "/TelegramBotDictionary/pyTranslatorBot1.py", line 57, in bot_message
+    translated_text = translator.translate(message.text, src=src, dest=dest).text
+  File "/TelegramBotDictionary/tenv/lib/python3.10/site-packages/googletrans/client.py", line 182, in translate
+    data = self._translate(text, dest, src, kwargs)
+  File "/TelegramBotDictionary/tenv/lib/python3.10/site-packages/googletrans/client.py", line 78, in _translate
+    token = self.token_acquirer.do(text)
+  File "/TelegramBotDictionary/tenv/lib/python3.10/site-packages/googletrans/gtoken.py", line 194, in do
+    self._update()
+  File "/TelegramBotDictionary/tenv/lib/python3.10/site-packages/googletrans/gtoken.py", line 62, in _update
+    code = self.RE_TKK.search(r.text).group(1).replace('var ', '')
+AttributeError: 'NoneType' object has no attribute 'group'
+(tenv) dato@Windows-Asus:~/TelegramBotDictionary$ python pyTranslatorBot1.py
+Traceback (most recent call last):
+  File "/TelegramBotDictionary/pyTranslatorBot1.py", line 73, in <module>
+    bot.polling(none_stop=True)
+  File "/TelegramBotDictionary/tenv/lib/python3.10/site-packages/telebot/__init__.py", line 1178, in polling
+    self.__threaded_polling(non_stop=non_stop, interval=interval, timeout=timeout, long_polling_timeout=long_polling_timeout,
+  File "/TelegramBotDictionary/tenv/lib/python3.10/site-packages/telebot/__init__.py", line 1253, in __threaded_polling
+    raise e
+  File "/TelegramBotDictionary/tenv/lib/python3.10/site-packages/telebot/__init__.py", line 1215, in __threaded_polling
+    self.worker_pool.raise_exceptions()
+  File "/TelegramBotDictionary/tenv/lib/python3.10/site-packages/telebot/util.py", line 150, in raise_exceptions
+    raise self.exception_info
+  File "/TelegramBotDictionary/tenv/lib/python3.10/site-packages/telebot/util.py", line 93, in run
+    task(*args, **kwargs)
+  File "/TelegramBotDictionary/tenv/lib/python3.10/site-packages/telebot/__init__.py", line 8707, in _run_middlewares_and_handler       
+    result = handler['function'](message)
+  File "/TelegramBotDictionary/pyTranslatorBot1.py", line 50, in bot_message
+    translated_text = translator.translate(message.text, src=src, dest=dest).text
+  File "/TelegramBotDictionary/tenv/lib/python3.10/site-packages/googletrans/client.py", line 182, in translate
+    data = self._translate(text, dest, src, kwargs)
+  File "/TelegramBotDictionary/tenv/lib/python3.10/site-packages/googletrans/client.py", line 78, in _translate
+    token = self.token_acquirer.do(text)
+  File "/TelegramBotDictionary/tenv/lib/python3.10/site-packages/googletrans/gtoken.py", line 194, in do
+    self._update()
+  File "/TelegramBotDictionary/tenv/lib/python3.10/site-packages/googletrans/gtoken.py", line 62, in _update
+    code = self.RE_TKK.search(r.text).group(1).replace('var ', '')
+AttributeError: 'NoneType' object has no attribute 'group'
